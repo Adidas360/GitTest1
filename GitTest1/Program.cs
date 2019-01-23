d25f3f8122ee90becd75fecad948b634a40a6d70
Fredrik Karlsson <cyberman_no1@hotmail.com>
2019-01-23 14:15:32 +0100

    Lägger till "Fredrik1"

--- GitTest1/Program.cs
+++ GitTest1/Program.cs
@@ -7,6 +7,7 @@ namespace GitTest1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Fredrik1");
         }
     }
 }
